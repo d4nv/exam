e01739df63db0bcca8efba1aadcf5f481ab5c662
d4nv <g00435140@atu.ie>
2024-11-04 09:45:47 +0000

    exam

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,28 @@
+package org.example;
+
+
+public class Product {
+    public static void main(String[] args) {
+        Product user = new Product();
+
+        user.setProductId(a);
+        user.setname(b);
+        user.setprice(v);
+        user.setquantity(d);
+
+        String ProductId = user.getProdcutId();
+        String Name = user.getname();
+        String price = user.getprice();
+        String quantity = user.getquantity();
+
+
+
+
+
+
+
+
+
+
+    }
+}
